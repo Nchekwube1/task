e2954bbd11951d0a330f39cce11d3fb6c828807c
Nchekwube1 <unekwenchekwube@gmail.com>
2021-11-30 15:11:48 +0100

    task 2 done

--- Logic.js
+++ Logic.js
@@ -13,4 +13,20 @@ function Arithmetic(a, b, c) {
         return (+a * +b);
     }
 }
-console.log(Arithmetic("6", "9", "*"));
+// console.log(Arithmetic("6","9","*"))
+// Task two
+function ScoreKeeper(a, b) {
+    if (a.length !== b.length)
+        return;
+    var scoreA = 0;
+    var scoreB = 0;
+    for (var i = 0; i < a.length; i++) {
+        // let j=0
+        // console.log(a[i], b[j])
+        // console.log(a, b)
+        // if (a[i] !== b[i])  { scoreA= scoreA scoreB= scoreB} 
+        a[i] > b[i] ? scoreA++ : a[i] < b[i] ? scoreB++ : null;
+    }
+    return [scoreA, scoreB];
+}
+console.log(ScoreKeeper([0, 0, 1], [0, 0, 0]));
--- Logic.ts
+++ Logic.ts
@@ -16,4 +16,27 @@ function Arithmetic(a:string, b:string, c:string):Number{
 }
 
 
-console.log(Arithmetic("6","9","*"))
+// console.log(Arithmetic("6","9","*"))
+
+
+// Task two
+
+
+function ScoreKeeper(a:number[], b:number[]){
+    if a.length !== b.length  return
+     let scoreA = 0
+     let scoreB = 0
+    for(let i=0; i<a.length;i++){
+        // let j=0
+            // console.log(a[i], b[j])
+            // console.log(a, b)
+            // if (a[i] !== b[i])  { scoreA= scoreA scoreB= scoreB} 
+             a[i]>b[i] ? scoreA ++ :  a[i]<b[i]?  scoreB++ : null
+            
+   
+    }
+
+    return [scoreA,scoreB]
+}
+
+console.log(ScoreKeeper([0,0,1],[0,0,0]))
